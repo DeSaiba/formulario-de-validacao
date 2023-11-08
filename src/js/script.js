e8c8d4844655c7c940a5028ec8b29e8d9202b95c
Vinicius de Saiba <viniciusdesaiba@gmail.com>
2023-11-08 16:34:34 -0300

    add função verifica tecla enter

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
-const inputItems = document.querySelectorAll('.inputItem');
+const inputItems = document.querySelectorAll ('.inputItem');
 const btnSend = document.querySelector('.btn-send');
 const warnings = document.querySelectorAll('.warning');
+
 
 function updateFieldValidation(item) {
     if (item.value !== "") {
@@ -15,22 +16,15 @@ function updateFieldValidation(item) {
 function validateFields() {
     let allFieldsValid = true;
 
-    inputItems.forEach((item) => {
+    inputItems.forEach((item, i) => {
+        updateFieldValidation(item);
         if (item.value === "") {
-            item.classList.add("border-red");
             allFieldsValid = false;
+            warnings[i].classList.remove("warning");
+            warnings[i].classList.add("opacity");
         } else {
-            item.classList.remove("border-red");
-        }
-    });
-
-    warnings.forEach((opacity, i) => {
-        if (inputItems[i].value === "") {
-            opacity.classList.add("opacity");
-            opacity.classList.remove("warning");
-        } else {
-            opacity.classList.remove("opacity");
-            opacity.classList.add("warning");
+            warnings[i].classList.remove("opacity");
+            warnings[i].classList.add("warning");
         }
     });
 
@@ -38,16 +32,24 @@ function validateFields() {
 }
 
 inputItems.forEach((item) => {
-    item.addEventListener("change", () => {
+    item.addEventListener("blur", () => {
         updateFieldValidation(item);
     });
 });
 
-btnSend.addEventListener("click", () => {
-    const allFieldsValid = validateFields();
+btnSend.addEventListener("click", (e) => {
+    e.preventDefault(); 
 
-    if (allFieldsValid) {
-        // O código aqui é executado quando todos os campos estão preenchidos corretamente.
-        // Posso adicionar a lógica para enviar o formulário ou realizar outra ação aqui.
+    if (validateFields()) {
+        // Todos os campos estão preenchidos corretamente, continue com o envio do formulário
+        // form.submit(); // Envie o formulário (descomente esta linha para permitir o envio)
     }
 });
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault(); // Impede a ação padrão de "Enter" (como enviar um formulário)
+        btnSend.click(); // Simula um clique no botão de envio
+    }
+});
+
